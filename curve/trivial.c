25e50e21726b88d9b32cb560731c48b44450a6ac
James W. McClain <jmcclain@daystrom-data-concepts.com>
2017-06-11 15:32:35 -0400

    Add Trivial Curve

---
+++ curve/trivial.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2010-2014 and 2016-2017, James McClain and Mark Pugner
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgement: This product
+ *    includes software developed by Dr. James W. McClain and Dr. Mark
+ *    C. Pugner.
+ * 4. Neither the names of the authors nor the names of the
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "curve.h"
+
+#define ONES ((uint32_t)(-1))
+#define DOUBLE_TO_BITS(x) (uint32_t)(ONES*x)
+#define BITS_TO_DOUBLE(b) (((double)b)/ONES)
+
+
+uint64_t xy_to_curve(double x, double y) {
+  uint64_t x_bits = DOUBLE_TO_BITS(x);
+  uint64_t y_bits = DOUBLE_TO_BITS(y);
+  return (x_bits << 32) | (y_bits);
+}
+
+void curve_to_xy(uint64_t d, double * x, double * y) {
+  uint32_t x_bits = 0, y_bits = 0;
+  x_bits = d >> 32;
+  y_bits = ONES & d;
+  *x = BITS_TO_DOUBLE(x_bits);
+  *y = BITS_TO_DOUBLE(y_bits);
+}
